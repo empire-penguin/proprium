32d05d8752b05f403a31e0ef2e3b670233879ea2
Gavtriple9 <29155514+Gavtriple9@users.noreply.github.com>
2024-03-09 23:01:24 -0800

    add transformation to library

--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,3 @@
-pub use proprium_core::{Mat2x2, Mat3x3, Mat4x4, Matrix, Point3D, Vec2D, Vec3D, Vec4D, Vector};
+pub use proprium_core::{
+    Mat2x2, Mat3x3, Mat4x4, Matrix, Point3D, Transformation, Vec2D, Vec3D, Vec4D, Vector,
+};
